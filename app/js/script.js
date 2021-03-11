543a915483b45f7cf306700be00e1435c4638a4b
thibault <thibault.deverge@gmail.com>
2021-03-11 12:58:09 +0700

    close Menu by click out

--- app/js/script.js
+++ app/js/script.js
@@ -3,31 +3,40 @@ const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".had-fade");
 
-const closeMenu = function (e) {
-  const x = e.target.closest(".header__menu");
-  if (x === header) console.log("youhouu");
-};
+///////////////////////
+// Helper function Open and Close menu
 
-btnHamburger.addEventListener("click", function () {
+function openMenu() {
+  document.body.classList.add("noscroll");
+  header.classList.add("open");
+  fadeElems.forEach(element => {
+    element.classList.add("fade-in");
+    element.classList.remove("fade-out");
+  });
+}
+
+function closeMenu() {
+  document.body.classList.remove("noscroll");
+  header.classList.remove("open");
+  fadeElems.forEach(element => {
+    element.classList.remove("fade-in");
+    element.classList.add("fade-out");
+  });
+}
+
+///////////////////////
+//  Event listener
+
+overlay.addEventListener("click", function (e) {
   if (header.classList.contains("open")) {
-    // Remove the overlay
-    document.body.classList.remove("noscroll");
-    header.classList.remove("open");
-    fadeElems.forEach(element => {
-      element.classList.remove("fade-in");
-      element.classList.add("fade-out");
-    });
-  } else {
-    // Add the overlay
-    document.body.classList.add("noscroll");
-    document.addEventListener("click", closeMenu);
-    header.classList.add("open");
-    fadeElems.forEach(element => {
-      element.classList.add("fade-in");
-      element.classList.remove("fade-out");
-    });
+    closeMenu();
   }
 });
 
-// Evenement au click, si le click à lieu sur un élément dont le plus proche parent à la classe 'open', return '';
-// Sinon, remove la classe open du header et ferme le menu
+btnHamburger.addEventListener("click", function (e) {
+  if (header.classList.contains("open")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
